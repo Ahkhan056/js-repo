3b17156c181c54eecf39b1d497c7b424b8dfb29d
Abdul Haq <ahkhan0566@gmail.com>
2025-04-26 17:47:18 +0000

    variables-code-practice

---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const empId=1234
+
+let empName = "ali"
+
+var empEmail= "ali@gmail.com"
+
+empAccount= "HBL bank"
+
+let empState;
+
+console.table([empId,empName, empEmail, empAccount, empState])
