035a0772c0fa54458764ce44d4c0df1534e2a7a4
Abdul Haq <ahkhan0566@gmail.com>
2025-04-27 05:47:01 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,28 @@
+ " use strict "   // treat all code as new version of JS
+
+ alert(4+5) //this is ok but in browser and now we use nodeJs so its not ok and we use console.log also we use it but later for some other things..
+
+ console.log("hello world")
+
+ console.log("abdullah")
+
+ let name= "abdullah" // let is a keyword to declare a variable
+
+ let age = 20 // let is a keyword to declare a variable
+
+    let isMarried = false // let is a keyword to declare a variable 
+
+ //  datatypes are multiple in JS ..=>
+    // nummber but from => 0 to 2 power 53. 
+
+// string => "hello world" or 'hello world' or `hello world`
+// boolean => true or false
+// null => empty value / stanalone value 
+// undefined => value not defined .. like a variable not assigned any value.
+// object => collection of key value pairs.
+// symbol => unique value and not equal to any other value.
+
+// BigInt => for large numbers like 1234567890123456789012345678901234567890..
+
+console.log (typeof  null);
+
--- basics/variables.js
+++ basics/variables.js
@@ -8,4 +8,6 @@ empAccount= "HBL bank"
 
 let empState;
 
+/* we can  not use use var in today world because of its scope in function and blocks like it is able to edit even outside the class or function*/
+
 console.table([empId,empName, empEmail, empAccount, empState])
