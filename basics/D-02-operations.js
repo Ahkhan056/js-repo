2b373c5540d29aa836f36826a3f8eaa9a552c83e
Abdul Haq <ahkhan0566@gmail.com>
2025-04-27 13:46:46 +0000

    learned operations in js specially on strings

---
+++ basics/D-02-operations.js
@@ -0,0 +1,36 @@
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%2)
+console.log(2**2)
+
+
+let str1 = "hello"
+
+let str2= "  abdullah"
+
+console.log(str1 + str2)
+
+
+console.log("1"+ 2); // when we declare string first then all numbers will be treated as string and on console it should be showed as string not any operation ill be done upon it..
+
+console.log(1+3 +"2");  // in this case  1+3 will be calculated first and then 2 will be added to it as string so the output will be 42
+
+console.log(true + 1);     // true is treated as 1 and false is treated as 0 so the output will be 2
+console.log(+true);      // + true will be converted to number so the output will be 1
+console.log(+"");     // empty string will be converted to 0 so the output will be 0
+
+let num1,num2, num3
+num1=num2= num3=2+2
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter
+
+console.log(gameCounter);
+
+
+
+
+
