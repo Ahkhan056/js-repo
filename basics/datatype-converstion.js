43b1f00450e97b14a9fac06674d7e0cf8a7c15be
Abdul Haq <ahkhan0566@gmail.com>
2025-04-27 07:10:56 +0000

    converstion-of-datatypes

---
+++ basics/datatype-converstion.js
@@ -0,0 +1,42 @@
+//   let score =33    // "33" convert to string
+
+//   let score= "33abc"  // convert to number
+// let score = null // convert to number
+
+
+// let score = undefined // convert to number
+ let score = true // convert to number
+
+
+   //console.log(typeof score) 
+
+  //console.log(typeof (score));
+  
+  let valueInNumber=Number(score); 
+ // console.log( typeof valueInNumber);
+   // console.log(valueInNumber); 
+  
+
+    // let value= 1
+
+    let value=""
+
+
+       // 1 in  boolean is true 
+       // 0 in boolean is false
+       // ""  in boolean is false
+       // "ali" in boolean is true..
+
+
+    let valueInValue= Boolean (value)
+
+  //  console.log(valueInValue);
+  //  console.log(typeof valueInValue);
+    
+
+
+    let number= 44  // convert number to string but the type is also string..
+    let stringNumber= String (number)
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
