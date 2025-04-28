8a2c575742e72079f7237e4126c00fe250be5347
Abdul Haq <ahkhan0566@gmail.com>
2025-04-28 17:05:31 +0000

    comparison between different datat  types

---
+++ basics/D-03-comparisonDt.js
@@ -0,0 +1,19 @@
+// console.log(2==2);
+// console.log(2<=2);
+// console.log(2!=2);
+
+//simple converstion in java and also this is best practice to use.
+
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);  // comparison >= and equality operators are different things in js .. as there is no type conversion in equality operator..
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);  // comparison >= and equality operators are different things in js .. as there is no type conversion in equality operator..
+
+console.log("2"===2); // strict equality operator.doen and check the type of the variable and also value of the variable.
+
