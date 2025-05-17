e78cd770a544e97a889aea542d26b962e042a501
Abdul Haq <ahkhan0566@gmail.com>
2025-05-17 14:04:54 +0000

    while and do while loop

---
+++ advance-js3/D-14-doWhileLoop.js
@@ -0,0 +1,26 @@
+// while loop 
+let i=1
+while (i<=10){
+    console.log(`the value of index is ${i} `);
+      i=i+2;
+    
+}
+
+
+// while loop on array
+
+const myarr=["helo","alo","malo"];
+let j=0;
+while (j< myarr.length){
+    console.log(`value of array is ${myarr[j]}`);
+    j++
+    
+}
+
+// do while loop
+let x=10;
+do{
+console.log(`value of x is ${x}`);
+x++
+}while(x<=10)
+    
